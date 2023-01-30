35b6eeee7946b19159ca4c3615455dd8fd403aab
ars-4 <ars7.dev@gmail.com>
2023-01-30 17:55:56 +0500

    External files moved to folder

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const server = require('./server');
-const conf = require('./configuration.json');
+const server = require('./resources/server');
+const conf = require('./resources/configuration.json');
 
+process.env.test = "String";
+
+console.log(process.env.test)
 
 let port = conf.defaults.port
 server.listen(port, () => {
