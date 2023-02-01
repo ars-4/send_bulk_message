e99991d40983763aacc7021afef6479c856069f7
ars-4 <ars7.dev@gmail.com>
2023-02-02 03:21:52 +0500

    Country Codes

--- resources/test.js
+++ resources/test.js
@@ -1,32 +1,5 @@
-const writeXlsxFile = require('write-excel-file/node')
+let mobile = "+1(123)656899";
 
-let row_1 = [{
-    value: 'Name',
-    fontWeight: 'bold'
-}]
-
-let test_row = [
-    [
-        [0, 1],
-        [0, 2],
-        [0, 3],
-        [0, 4]
-    ]
-]
-
-for(let i = 0; i < test_row.length; i++) {
-    for(let j = 0; j < test_row[i].length; j++) {
-        test_row[i][j][1]
-    }
+if(String(mobile).includes('(')) {
+    console.log(mobile.replace('(', '').replace(')', ''))
 }
-
-data = [
-    row_1,
-    [{value:'John Smithson'}]
-]
-
-writeXlsxFile(data, {
-    filePath: './resources/uploads/file.xlsx'
-}).then(() => {
-    console.log("Done")
-})
